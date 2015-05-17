aed874a354726aea2cc2e06afea656cef058d435
zhxfl <291221622@qq.com>
2015-05-17 13:30:35 +0800

    rm CMakeFiles

--- tools/CMakeFiles/cifar10_data.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-FILE(REMOVE_RECURSE
-  "../test/cifar10_data.pdb"
-  "../test/cifar10_data"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/cifar10_data.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
--- tools/CMakeFiles/cifar10_data.o.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/cifar10_data.o.dir/cifar10_data.cpp.o"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/cifar10_data.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
