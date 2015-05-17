6f9902eadaf16fdbec1aa595c6ea7aca6229e83c
zhxfl <291221622@qq.com>
2015-05-17 13:29:59 +0800

    conv data on cifar-10

---
+++ tools/CMakeFiles/cifar10_data.o.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cifar10_data.o.dir/cifar10_data.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cifar10_data.o.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
