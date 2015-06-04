9eaa44b6057e9fee89f0348b2bc8dd03c97cfc93
zhxfl <huanxinzheng@tuputech.com>
2015-06-04 14:51:59 +0800

    fix bug for multi_view

--- composite/graph/fetch_image.hpp
+++ composite/graph/fetch_image.hpp
@@ -5,6 +5,12 @@
 #include <vector>
 #include <utility>
 #include "dispatch/runnable.hpp"
+#include "dispatch/op.hpp"
+#include "operations/include/image_label.hpp"
+#include "composite/graph/fetch_image.hpp"
+#include "composite/graph/split.hpp"
+#include "composite/graph/copy.hpp"
+#include "composite/vectorize.hpp"
 
 using namespace std;
 
@@ -35,7 +41,9 @@ namespace purine {
             virtual ~FetchImage() override {}
             const vector<Blob*>& images() { return images_; }
             const vector<Blob*>& labels() { return labels_; }
-            const Blob* get_master_cpu_labels() { return master_cpu_labels;}
+            Blob* get_master_cpu_labels() { 
+                return master_cpu_labels;
+            }
     };
 }
 
